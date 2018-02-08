1c61ec8b93bbe084d6c64113119fb528cef4925f
booknap <napasai45@gmail.com>
2018-02-08 11:16:30 +0700

    updated

--- src/SetPractice.java
+++ src/SetPractice.java
@@ -12,6 +12,11 @@ public class SetPractice {
 	public static void addAll(Set<String> set, String ...values) {
 		// add all the values to the set. 
 		// print each one as you add it
+		for(String arg:values){
+			set.add(arg);
+			System.out.println("add "+arg);
+		}
+		System.out.println();
 	}
 	
 	/**
@@ -20,6 +25,12 @@ public class SetPractice {
 	public static void print(Set<String> set) {
 		// create an Iterator using set.iterator()
 		// then use the iterator to print everything in the set
+		System.out.println("Set contains: ");
+		Iterator<String> iter = set.iterator();
+		while(iter.hasNext()){
+			String next = iter.next();
+			System.out.println(next);
+		}
 	}
 	
 	/**
@@ -27,45 +38,57 @@ public class SetPractice {
 	 */
 	public static void setExplorer( ) {
 		// 1. Create a HashSet and add strings: dog, ant, bird, elephant, cat
-		
+		Set<String> set = new HashSet<String>();
+		addAll(set, "dog","ant","bird","elephant","cat");
 		
 		// 2. Print elements in the set using an Iterator over the Set.
 		// You're going to need to do this again, so write a method print(Set set) to do it.
-		
+		print(set);
+		System.out.println();
 		
 		// 3. are the elements printed in same order than you added them? 
 		// What does this tell you about Sets?
-		
+		System.out.println("Question: Are the elements pritnted in same order that you added them?");
+		System.out.println();
 		
 		// 4. Remove all the elements from the set, using a method that 
 		// defined in Set (and Collection). What is the method to use?
-		
+		System.out.println("Clearing the set");
+		set.clear();
+		System.out.println();
 		
 		// 5. Add same elements as exercise 1, but in a different order,
 		// such as: "cat", "elephant", "bird", "ant", "dog"
-		
+		addAll(set, "cat", "elephant", "bird", "ant", "dog");
+		System.out.println();
 		
 		// 6. Print the elements in the set.  Is the order same as before?
-		
+		print(set);
+		System.out.println();
 		
 		// 7. Add duplicate elements to the set.  Add another "cat" and "dog".
 		// To ensure that these are *distinct* objects use 'set.add(new String("cat"))'
 		// and new String("dog").
 		System.out.println("Add duplicate items to the set ");
+		System.out.println();
 		
 		
 		// 8. Print the elements in set.  Are there any duplicates?
 		// What property of Set does this demonstrate?
-		System.out.println("Set contains: ");
-		
+		print(set);
+		System.out.println();
 		
 		// 9. Repeat the exercises using a TreeSet instead of HashSet.
 		//  What is different when you use TreeSet?
+		Set<String> Sett = new TreeSet<String>();
+		addAll(Sett, "dog","ant","bird","elephant","cat");
+		System.out.println();
 		
 		// Hint: Use Polymorphism.  Make the Set object be a *parameter* to the
 		// method so you can easily use a different set just by changing the parameter!
 		
 		// 10. Repeat again using TreeSet with a Comparator.
+		print(Sett);
 	}
 
 	public static void main(String[] args) {
